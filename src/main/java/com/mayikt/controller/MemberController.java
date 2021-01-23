60cdb7f10bec0d5e9db46f2ef89c9e8e7c93478b
wqezr <18912889306@163.com>
2021-01-23 13:56:22 +0800

    自定义YML配置

--- src/main/java/com/mayikt/controller/MemberController.java
+++ src/main/java/com/mayikt/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.mayikt.member.mapper.MemberMapper;
 import com.mayikt.member.service.MemberServcice;
 import com.mayikt.order.mapper.OrderMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +18,17 @@ public class MemberController {
     @Autowired
     private MemberServcice memberServcice;
 
+    @Value("${com.wqezr.userName}")
+    private String userName;
+
     @RequestMapping("/addUser")
     public String addUser(String name){
         return memberServcice.addUser(name)?"success":"fail";
+    }
+
+
+    @RequestMapping("/getUserName")
+    public String getUserName(){
+        return userName;
     }
 }
